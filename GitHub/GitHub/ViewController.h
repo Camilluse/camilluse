24116aedbdd20b5d2f44dcbbdae4e59ec3d39d5d
camilluse <498514357@qq.com>
2015-08-14 10:04:23 +0800

    添加工程

---
+++ GitHub/GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub
+//
+//  Created by 闪冲 on 15/8/14.
+//  Copyright (c) 2015年 闪冲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitHub/GitHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitHub
+//
+//  Created by 闪冲 on 15/8/14.
+//  Copyright (c) 2015年 闪冲. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
